340d75a2a5bd00221efaa772e88f37db4143bf47
Alex Bielen <alexhendriebielen@gmail.com>
2025-06-19 14:52:06 -0400

    Fix: unintentionally disabling VSCode and hiding settings.
    See: https://github.com/alexbielen/home/commit/d4bf4e17a0cccbe9e4c9a11495504f435f726785
    See: https://code.visualstudio.com/docs/configure/profiles
    
    Why: I unintentionally added the `profiles.default` to `programs.vscode`
    instead of to `keybindings`, `extensions`, amd `userSettings`. These
    were moved into `profiles.<x>` so that you can choose different profiles
    based on the project that you're working on. This is a cool feature that
    I might actually want to take advantage of in the future (e.g., `Data
    Science Profile` or `Rust` etc.
    
    Anyway, this was tricky to figure out because I unenabled VSCode so it
    wasn't showing up at all. That should've been a sign that I unenabled it
    unintentionally. For example, `code` wasn't showing up in the command
    line.
    
    How: Fix the `vscode-config.nix` file.
    
    Tags: vscode, nix, profiles

--- application-config/vscode/vscode-config.nix
+++ application-config/vscode/vscode-config.nix
@@ -1,43 +1,47 @@
 { pkgs, ... }:
 {
-  programs.vscode.profiles.default = {
+  programs.vscode = {
     enable = true;
 
     # we don't want anything else writing to the extensions directory
     mutableExtensionsDir = false;
 
-    userSettings = {
-      # this attrset generates the settings.json file in VSCODE
-      editor.formatOnSave = true;
-      workbench.colorTheme = "Nord";
+    profiles.default = {
+
+      userSettings = {
+        # this attrset generates the settings.json file in VSCODE
+        editor.formatOnSave = true;
+        workbench.colorTheme = "Nord";
+      };
+
+      keybindings = [
+        {
+          key = "shift+cmd+j";
+          command = "workbench.action.focusActiveEditorGroup";
+          when = "terminalFocus";
+        }
+      ];
+
+      # TODO: add extensions to the default profile
+      extensions = with pkgs.vscode-marketplace; [
+        # ms-vscode.cpptools (this was removed?)
+        arcticicestudio.nord-visual-studio-code
+        bmalehorn.vscode-fish
+        dbaeumer.vscode-eslint
+        github.copilot
+        github.vscode-github-actions
+        golang.go
+        jnoortheen.nix-ide
+        ms-python.black-formatter
+        ms-python.debugpy
+        ms-python.flake8
+        ms-python.python
+        ms-vscode.cmake-tools
+        ms-vscode.cpptools-extension-pack
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
+        vscodevim.vim
+      ];
     };
-
-    keybindings = [
-      {
-        key = "shift+cmd+j";
-        command = "workbench.action.focusActiveEditorGroup";
-        when = "terminalFocus";
-      }
-    ];
-
-    extensions = with pkgs.vscode-marketplace; [
-      # ms-vscode.cpptools (this was removed?)
-      arcticicestudio.nord-visual-studio-code
-      bmalehorn.vscode-fish
-      dbaeumer.vscode-eslint
-      github.copilot
-      github.vscode-github-actions
-      golang.go
-      jnoortheen.nix-ide
-      ms-python.black-formatter
-      ms-python.debugpy
-      ms-python.flake8
-      ms-python.python
-      ms-vscode.cmake-tools
-      ms-vscode.cpptools-extension-pack
-      rust-lang.rust-analyzer
-      tamasfe.even-better-toml
-      vscodevim.vim
-    ];
   };
 }
